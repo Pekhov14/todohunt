444eb3867fdbaae3920c9f000806aa090aef2adb
pekhov <pehovanton21@gmail.com>
2025-10-14 23:32:40 +0300

    Create display help

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,41 @@
+use std::{fs, process};
 use std::fs::File;
-use std::io::{Write};
+use std::io::{BufRead, Read, Write};
 use std::path::Path;
+use std::env;
 
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config_file = "todohunt.toml";
+
+    if Path::new(config_file).exists() {
+        // read config
+        println!("Config file already exists: {}", config_file);
+    } else {
+        create_default_config()?;
+        println!("Creating config file: {}", config_file);
+    }
+
+    let args = env::args().collect::<Vec<String>>();
+
+    if args.len() < 2 {
+        display_help();
+        process::exit(0);
+    }
+
+    let command = &args[1];
+
+    dbg!(command);
+
+    match command.as_str() {
+        "scan" => { println!("Scanning project"); },
+        "init" => { println!("Initializing..."); },
+        "info" => display_help(),
+        _ => { eprint!("{} is not a valid provided command", command); process::exit(1); }
+    }
+
+
+    Ok(())
+}
 
 fn create_default_config() -> Result<(), Box<dyn std::error::Error>> {
     let mut config_file = File::create("todohunt.toml")?;
@@ -9,6 +43,7 @@ fn create_default_config() -> Result<(), Box<dyn std::error::Error>> {
     let config_content = r#"[general]
 ignore = ["vendor", "node_modules", ".idea", ".vscode"]
 ignore_file_types = [".env"]
+work_dir = ["src"] # ./
 
 [provider]
 name = "github"
@@ -27,17 +62,14 @@ open_links_in = "vscode" # idea
     Ok(())
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let config_file = "todohunt.toml";
-
-    if Path::new(config_file).exists() {
-        // read config
-        println!("Config file already exists: {}", config_file);
-    } else {
-        // Init config file
-        create_default_config()?;
-        println!("Creating config file: {}", config_file);
-    }
-
-    Ok(())
+fn display_help() -> () {
+    println!("\n🦀 todohunt — hunting for TODOs\n");
+    println!("\nUsage: todohunt [COMMAND] [OPTIONS]\n");
+    println!("Commands:");
+    println!("  scan    Scan for TODOs in the project");
+    println!("  init    Create default config file");
+    println!("  help    Show this help message\n");
+    println!("Examples:");
+    println!("  todohunt scan    # Scan project for TODOs");
+    println!("  todohunt init    # Create config file");
 }
