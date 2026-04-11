2a7e28e7fdf5a2456e2bd4e00dcff9cead8cb5eb
Pekhov Anton <pehovanton21@gmail.com>
2026-04-11 19:52:52 +0300

    Add lib for cli (#1)
    * Init cobra lib
    
    * Add description for commands

---
+++ cmd/root.go
@@ -0,0 +1,42 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "todohunt",
+	Short: "Scan your project for TODO comments",
+	Long: `Todohunt is a lightweight CLI that helps you find TODO comments
+across your project and keep track of unfinished work.
+
+Use it from the root of your repository to quickly inspect the codebase
+and surface tasks, notes, and technical debt markers before they get lost.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.todohunt.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
